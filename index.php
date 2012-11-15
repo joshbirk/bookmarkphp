a66cd72870be2d2ae8bf2ae892abcf82b9318fc1
joshbirk <joshua.birk@salesforce.com>
2012-11-15 09:40:10 -0600

    index

--- index.php
+++ index.php
@@ -1,5 +1,28 @@
 <?
+	//URL of targeted site  
+	$url = urldecode($_GET["u"]);  
+	$ch = curl_init();  
 
- phpinfo();
+	// set URL and other appropriate options  
+	curl_setopt($ch, CURLOPT_URL, $url);  
+	curl_setopt($ch, CURLOPT_HEADER, 0);  
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);  
+
+	// grab URL and pass it to the browser  
+
+	$output = curl_exec($ch);
+ 
+	$DOM = new DOMDocument;
+    $DOM->loadHTML($output);
+
+    //get the title
+	$items = $DOM->getElementsByTagName('title');
+
+	//display all H1 text
+	$title = $items[0];
+	
+	preg_match('\$\d+(\.\d+)?',
+	    $output, $matches);
 
 ?>
+<?echo $url?>|<?echo $title?>|<?echo $matches[0]?>
